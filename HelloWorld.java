341a102326ca5ec7c228f3dc8b963aa245b1883f
dhoniworld <161342865+dhoniworld@users.noreply.github.com>
2024-03-14 21:45:38 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-class HelloWorld {
-public static void main(String[] args)
-{
-System.out.println("Hello World!") ;
-System.out.println("welcome to STARKWORLD!") ;
-System.out.println("welcome to GOTHAM CITY!") ;
-}
-}
