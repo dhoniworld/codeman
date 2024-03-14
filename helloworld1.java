46912b0a3a16d8269835cc577de9c7c64aef452f
unknown <vishnuchandra71@gmail.com>
2024-03-14 21:43:19 +0530

    first-commit

---
+++ helloworld1.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+public static void main(String[] args)
+{
+System.out.println("Hello World!") ;
+System.out.println("welcome to STARKWORLD!") ;
+System.out.println("welcome to GOTHAM CITY!") ;
+}
+}
