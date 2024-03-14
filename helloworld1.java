575829cfe77a32b980b08fdbeddf8cc22e91c009
dhoniworld <161342865+dhoniworld@users.noreply.github.com>
2024-03-14 22:47:01 +0530

    Update helloworld1.java

--- helloworld1.java
+++ helloworld1.java
@@ -2,7 +2,7 @@ class HelloWorld {
 public static void main(String[] args)
 {
 System.out.println("Hello World!") ;
-System.out.println("welcome to STARKWORLD!") ;
-System.out.println("welcome to GOTHAM CITY BATMAN!") ;
+System.out.println("welcome to STARKWORLD1112!") ;
+System.out.println("welcome to GOTHAM CITY BATMAN1122!") ;
 }
 }
